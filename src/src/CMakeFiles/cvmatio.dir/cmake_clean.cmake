de1c2f3b7555a44551f0a4c46472ffe342150492
cupwater <baoyun20@gmail.com>
2016-09-06 23:14:48 -0700

    first commit

---
+++ src/src/CMakeFiles/cvmatio.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cvmatio.dir/MatlabIO.cpp.o"
+  "CMakeFiles/cvmatio.dir/main.cpp.o"
+  "cvmatio.pdb"
+  "cvmatio"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cvmatio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
